4e676e33369a7fa91a83a7b54a65f551e9268955
Awais-404 <155368467+Awais-404@users.noreply.github.com>
2024-01-10 15:45:21 +0000

    i did stuff

--- Library.cpp
+++ Library.cpp
@@ -6,41 +6,63 @@ using namespace std;
 class books{
     string id,title,auther;
     public:
-    display_books();
-    sbytitle();
-    sbyauther();
-    sbycategory();
-    admin();
+    void display_books();
+    void sbytitle();
+    void sbyauther();
+    void sbycategory();
+    void admin();
 }obj;
 
-main(){
-    cout<<"Choose an action: \n
-    1-Show all books \n
-    2-Search by title \n
-    3-Search by auther \n
-    4-Choose category \n
-    5-ADMIN control \n
-    6-Exit program \n";
+int main(){
+    char op;
+    cout<<"Choose an action: \n";
+    cout<<"1-Show all books \n";
+    cout<<"2-Search by title \n";
+    cout<<"3-Search by auther \n";
+    cout<<"4-Choose category \n";
+    cout<<"5-ADMIN control \n";
+    cout<<"Esc-Exit program \n";
+    cin>>op;
     switch(op){
-        case 1{
-            display_books();
+        case 1:{
+            obj.display_books();
         }
-        case 2{
-            sbytitle();
+        case 2:{
+            obj.sbytitle();
         }
-        case 3{
-            sbyauther();
+        case 3:{
+        obj.sbyauther();
         }
-        case 4{
-            sbycategory();
+        case 4:{
+            obj.sbycategory();
         }
-        case 5{
-            admin();
+        case 5:{
+            obj.admin();
         }
-        case 6{
+        case 27:{
             return 0;
         }
     }
 
-return 0;
+    return 0;
+}
+
+void books :: display_books(){
+
+}
+
+void books :: sbytitle(){
+    
+}
+
+void books :: sbyauther(){
+    
+}
+
+void books :: sbycategory(){
+    
+}
+
+void books :: admin(){
+    
 }
